e55dbf0277cca793f2f4b7e75ee30a1830258492
wasimatta <151384826+wasimatta@users.noreply.github.com>
2023-11-22 16:00:58 +0200

    Update P1-Interactive_Monthly_Calendar_Application-1190699_1200920.asm

--- P1-Interactive_Monthly_Calendar_Application-1190699_1200920.asm
+++ P1-Interactive_Monthly_Calendar_Application-1190699_1200920.asm
@@ -10,17 +10,29 @@
 
 ################# Data segment #####################
 .data
-    file_path:    		.asciiz "C:\\Users\\tariq\\OneDrive\\Desktop\\P1-Arch\\calendar-input.txt" 	# The path of input file 
+    #file_path:    		.asciiz "C:\\Users\\tariq\\OneDrive\\Desktop\\P1-Arch\\calendar-input.txt" 	# The path of input file 
+    file_path:    		.asciiz "C:\\Users\\wasim\\Desktop\\calendar-input.txt" 	# The path of input file 
     menu:  	  		.asciiz "*-----------------------------*\n*           Main Menu          *\n*-----------------------------*\n[1]--> View the calendar\n[2]--> View Statistics\n[3]--> Add a new appointment\n[4]--> Delete an appointment\n[0]--> Exit\nEnter your choice: "
     view_menu:    		.asciiz "\n*-----------------------------*\n*          View Menu          *\n*-----------------------------*\n[1]--> View per day\n[2]--> View per set of days\n[3]--> View given slot in a given day\n[0]--> Back\nEnter your choice: "
     view_per_day_str:  		.asciiz "\nEnter the day (1-31): "
     view_per_set_days_str: 	.asciiz "\nEnter a comma-separated list of integers: "
     invalid_day_str:   		.asciiz "\nInvalid day input. Please enter a day between 1 and 31.\n"
     newline: 			.asciiz "\n"
+    char_L:       		.asciiz "L"
+    char_M:       		.asciiz "M"
+    char_O:       		.asciiz "O"
+    space:       		.asciiz "   "
+    number_of_L:		.asciiz "Number of lectures (in hours): "
+    number_of_OH:		.asciiz "number of OH (in hours): "
+    number_of_M	:		.asciiz "Number of Meetings (in hours): "
+    The_average:		.asciiz "The average lectures per day: "
+    The_ratio:		        .asciiz "The ratio between total number of hours reserved for L and the OH: "
 
+    
+    
     input_string: 		.space 91      		# Maximum length of the input string (we calculated it) 
-    calendar_mem: 		.space 2790     	# Memory space to store the calendar (we calculated it 31 × 10 × 9)
-
+    calendar_mem: 		.space 2790     	# Memory space to store the calendar (we calculated it 31 Ã— 10 Ã— 9)
+    END: 			.asciiz "E"
         
 ################# Code segment #####################
 .text
@@ -216,9 +228,199 @@ view_calendar:
 
 view_statistics:
     # Code to view statistics goes here
+	la $t3, calendar_mem 		# Load address of calendar_mem into register $t2
+
+	li $s0, 0                      #counter for L in H
+	li $s1, 0			#counter for OH in H
+	li $s2, 0			#counter for M in H
+	
+	  Statistics_loop:
+        lb $t6, 0($t3)           	# Load a byte from the calendar
+        lb $t0, char_L  		
+        beq $t6, $t0,L_inc		# check if the char = "L" 
+        lb $t0, char_O
+        beq $t6, $t0,OH_inc		# check if the char = "O" 
+        lb $t0, char_M
+        beq $t6, $t0,M_inc		# check if the char = "M" 
+        lb $t0, END
+        beq $t6, $t0,END_COUNT		#check if calender is end
+        continue:
+        addi $t3, $t3, 1          	# Move to the next position in memory
+        j Statistics_loop   	# Continue the loop
+
+
+
+	  L_inc:
+        move $a0,$t3			#take copy of addrees of calender  to function
+	jal my_function			#call the function to find sum of hours
+	add $s0 , $s0 , $v0 		# add the hours for the counter 
+	j continue 			# back to the loop 
+	
+	   OH_inc:
+	move $a0,$t3			#take copy of addrees of calender  to function
+	jal my_function			#call the function to find sum of hours
+	add $s1 , $s1, $v0 		# add the hours for the counter 
+	j continue			# back to the loop 
+	
+          M_inc:
+	 move $a0,$t3			#take copy of addrees of calender  to function
+	jal my_function			#call the function to find sum of hours
+	add $s2 , $s2, $v0 		# add the hours for the counter 
+	j continue			# back to the loop 
+
+
+           END_COUNT:
+	
+	 li $v0, 4          			
+   	 la $a0, number_of_L        # load the text 		
+   	 syscall				
+   	 li $v0,1  		    # print int
+	 move $a0, $s0 	            # load number of H  
+	 syscall	
+	 li $v0, 4          			
+   	 la $a0,newline   	    # add new line  		
+   	 syscall
+   	
+	 li $v0, 4          			
+   	 la $a0, number_of_OH        # load the text 		
+   	 syscall				
+   	 li $v0,1  		    # print int
+	 move $a0, $s1 	            # load number of H  
+	 syscall	
+	 li $v0, 4          			
+   	 la $a0,newline   	    # add new line  		
+   	 syscall
+   	 
+   	  li $v0, 4          			
+   	 la $a0, number_of_M        # load the text 		
+   	 syscall				
+   	 li $v0,1  		    # print int
+	 move $a0, $s2 	            # load number of H  
+	 syscall	
+	 li $v0, 4          			
+   	 la $a0,newline   	    # add new line  		
+   	 syscall
+	
+   	# li $s4 ,0                  # s4 to save the avarege
+	 li $s5 ,0                  # s4 to save the ratio
+	 
+	 #add $s4,$s4,$s0
+	 #add $s4,$s4,$s1
+	 #add $s4,$s4,$s2
+	 li $t1,31
+	 
+    	mtc1 $s0, $f0  
+    	cvt.s.w $f0, $f0 	    	# Convert the integer to a float in $f0                        	
+    	mtc1 $t1, $f1 
+    	cvt.s.w $f1, $f1	    	# Convert the integer to a float in $f1                       
+    	div.s $f12, $f0, $f1  
+    	
+ 	 li $v0, 4          			
+   	 la $a0, The_average       # load the text 		
+   	 syscall				
+   	  li $v0, 2 
+	 syscall	
+	 li $v0, 4          			
+   	 la $a0,newline   	    # add new line  		
+   	 syscall
+
+
+    	mtc1 $s0, $f0  
+    	cvt.s.w $f0, $f0 	    	# Convert the integer to a float in $f0                        	
+    	mtc1 $s1, $f1 
+    	cvt.s.w $f1, $f1	    	# Convert the integer to a float in $f1                      
+    	div.s $f12, $f0, $f1  
+    	
+ 	 li $v0, 4          			
+   	 la $a0, The_ratio      # load the text 		
+   	 syscall				
+   	  li $v0, 2 
+	 syscall	
+	 li $v0, 4          			
+   	 la $a0,newline   	    # add new line  		
+   	 syscall
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     # Jump back to the menu
     j main
+    
+    
+    
+    
+    
+    my_function:
+	move $t2,$a0			#take copy of addrees of calender 
+	subi $t2, $t2, 3         	# Move to the previce position in memory
+	lb $t5 , 0($t2)			#load char from calender
+	li $t7, 0x0000002d		#load "-" to $t7 
+	beq $t5,$t7 next		#check if char from calender = "-"
+	lb $t9,0($t2)			#load the second number in $t9
+	sub $t9, $t9, 48         	# Convert ASCII to decimal
+	addi $t2,$t2 ,1			# Move to the next position in memory to load the second digit
+	lb $t4,0($t2)			#load second digit
+	sub $t4, $t4, 48         	# Convert ASCII to decimal
+
+	mul $t9 ,$t9 ,10		#this mul to put the both digit in one register
+	add $t9 ,$t9 , $t4
+	 
+	subi $t2, $t2, 2		# Move to the previce position in memory
+	j second_number                # first number end go to the second
+	next:
+	addi $t2, $t2, 1     		# Move to the next position in memory
+	lb $t9,0($t2)			#load the number from calender 
+	sub $t9, $t9, 48         	# Convert ASCII to decimal
+	subi $t2, $t2, 1         	# Move to the previce position in memory
+	
+	second_number:                  # same of above but for second number 
+	subi $t2, $t2, 2         	# Move to the next position in memory
+	li $t7 ,0x00000020
+	lb $t5 , 0($t2)
+	beq $t5,$t7 next_2
+	lb $t8,0($t2)
+	sub $t8, $t8, 48         # Convert ASCII to decimal
+	addi $t2,$t2 ,1
+	lb $t4,0($t2)
+	sub $t4, $t4, 48         # Convert ASCII to decimal
+	mul $t8 ,$t8 ,10
+	add $t8 ,$t8 , $t4
+	addi $t2,$t2,1
+	j next_3
+	next_2:
+	addi $t2,$t2,1
+	lb $t8,0($t2)
+	sub $t8, $t8, 48         # Convert ASCII to decimal
+
+	
+ 	next_3:
+	li $t2 , 6  		#load 6 to $t2 to check if any number from 1 to 5 
+	blt $t8,$t2 Add1       # check if the first number 1-5 then add 12 
+	
+	j else_1               # if not go to else 
+	Add1:
+	addi $t8,$t8,12 	# add 12 to the number
+	
+	else_1:
+	blt $t9,$t2 Add2       # check the scecond number
+	
+	j else_2		# if not go to else_2
+	Add2:
+	addi $t9,$t9,12        # add 12 to the number
+	
+	else_2: 		# not the number is ready 
+	sub $v0,$t9,$t8		# find the long of L buy sub
+	jr $ra
 
 #  -------------------------------------------------
 
